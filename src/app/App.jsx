1db68628ae98bc1ca7d50a38c16885242a3ce695
fl4meh <3nazmathiel@gmail.com>
2026-04-28 21:59:26 +0800

    init commit

---
+++ src/app/App.jsx
@@ -0,0 +1,12 @@
+import { Header } from "./components/header"
+
+export default function Hi(){
+  return (
+    <div class="parent">
+      <Header />
+      <h1>hi</h1>
+    </div>
+  ) 
+}
+
+
---
+++ src/app/components/Header.jsx
@@ -0,0 +1,9 @@
+export function Header() {
+return (
+<header>
+<div>
+<h1>PartTime</h1>
+</div>
+</header>
+);
+}
